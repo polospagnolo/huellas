99bbd68880f064604c541010822715ae388559ab
Jose Antonio Rojas <joseantoniorojasrodriguez11@gmail.com>
2018-04-16 13:44:50 +0200

    agregando boton para unir

--- resources/views/mensual_tr.blade.php
+++ resources/views/mensual_tr.blade.php
@@ -1,10 +1,20 @@
 @php
-$retraso = getRetraso($empleado,$dia_completo[$key])
+    $retraso = getRetraso($empleado,$dia_completo[$key])
 
 @endphp
 @if(is_null($retraso))
-    <th class="{{$dia > 5 ? 'table-dark' : ''}}"></th>
+    <td class="{{$dia > 5 ? 'table-dark' : ''}} text-center" style="cursor: pointer;>
+        @if($dia < 6)
+            <a href="{{url('picado/ampliado/'.$empleado->nombre.'/'.$dia_completo[$key])}}">Ver</a>
+        @endif
+    </td>
 @else
-    <th class="{{$dia > 5 ? 'table-dark' : ''}} text-center {{$retraso->motivo->clase}}" style="cursor: pointer;" data-toggle="tooltip" title="{{$retraso->comentario}}">{{$retraso->motivo->id == 10 ? $retraso->created_at->format('H:i') : $retraso->motivo->letra}}</th>
+    <td class="{{$dia > 5 ? 'table-dark' : ''}} text-center {{$retraso->motivo->clase}}" style="cursor: pointer;"
+        data-toggle="tooltip"
+        title="{{$retraso->comentario}}">
+        @if($dia < 6)
+            <a href="{{url('picado/ampliado/'.$empleado->nombre.'/'.$dia_completo[$key])}}">{{$retraso->motivo->id == 10 ? $retraso->created_at->format('H:i') : $retraso->motivo->letra}}</a>
+        @endif
+    </td>
 @endif
 
